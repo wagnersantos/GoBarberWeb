ad73155c627a0d9bc79be0c3366e30568e36c0e5
wagnersantos <wagtvc@gmail.com>
2019-10-29 22:44:37 -0300

    fix store configuration

--- src/store.js
+++ src/store.js
@@ -4,19 +4,21 @@ import createSagaMiddleware from 'redux-saga';
 import reducers from './reducers';
 import sagas from './sagas';
 
-const sagaMonitor = process.env.NODE_ENV
-  ? console.tron.createSagaMonitor()
-  : null;
+const sagaMonitor =
+  process.env.NODE_ENV === 'development'
+    ? console.tron.createSagaMonitor()
+    : null;
 const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
 const middlewares = [sagaMiddleware];
 
-const composer = process.env.NODE_ENV
-  ? compose(
-      applyMiddleware(...middlewares),
-      console.tron.createEnhancer()
-    )
-  : compose(applyMiddleware(...middlewares));
+const composer =
+  process.env.NODE_ENV === 'development'
+    ? compose(
+        applyMiddleware(...middlewares),
+        console.tron.createEnhancer()
+      )
+    : compose(applyMiddleware(...middlewares));
 
 const store = createStore(reducers, composer);
 
