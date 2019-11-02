584c0087940ac18612a7a154ea1686d1cfdcea27
wagnersantos <wagtvc@gmail.com>
2019-11-02 12:27:22 -0300

    add styles in profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Form, Input } from '@rocketseat/unform';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  return (
+    <Container>
+      <Form>
+        <Input name="name" type="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+
+        <hr />
+        <Input
+          name="oldPassword"
+          type="password"
+          placeholder="Sua senha atual"
+        />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="submit">Atualizar</button>
+      </Form>
+
+      <button type="button">Sair</button>
+    </Container>
+  );
 }
