907feb5a0e838e513a3a472b7af8d8e5bcbacd28
wagnersantos <wagtvc@gmail.com>
2019-11-02 20:37:31 -0300

    add schedule

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,60 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
+
+const data = [
+  {
+    id: '1',
+    hour: '08:00',
+    provider: 'Wagner Santos',
+    available: false,
+    past: true
+  },
+  {
+    id: '2',
+    hour: '09:00',
+    provider: 'Wagner Santos',
+    available: true,
+    past: true
+  },
+  {
+    id: '3',
+    hour: '10:00',
+    provider: 'Wagner Santos',
+    available: true,
+    past: false
+  },
+  {
+    id: '4',
+    hour: '11:00',
+    provider: 'Wagner Santos',
+    available: false,
+    past: false
+  }
+];
 
 export default function Dashboard() {
-  return <h1>Dash</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>31 de Maio</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        {data.map((e) => (
+          <Time key={e.id} past={e.past} available={e.available}>
+            <strong>{e.hour}</strong>
+            <span>{e.provider}</span>
+          </Time>
+        ))}
+      </ul>
+    </Container>
+  );
 }
